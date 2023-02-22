aa2a4a9a5f622910b9728487c681ca974eed982f
PLANCQ Valentin <sulfeur84@gmail.com>
2023-02-22 15:54:27 +0100

    En fait ca va

--- Assets/Scripts/LoadSceneOnTrigger.cs
+++ Assets/Scripts/LoadSceneOnTrigger.cs
@@ -1,18 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadSceneOnTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string SceneName;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneName);
+        }
     }
 }
---
+++ Assets/Scripts/TheEnd.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheEnd : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+            Application.OpenURL(webplayerQuitURL);
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
